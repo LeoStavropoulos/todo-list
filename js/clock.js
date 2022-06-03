b319dcff0e7c18bbea3fb5bf47b5672b6981fc64
Leonardos <stavropoulos.leonardos@gmail.com>
2022-06-03 05:06:55 +0300

    created a clock element

---
+++ js/clock.js
@@ -0,0 +1,63 @@
+//Selectors
+const headerTop = document.querySelector(".header > .top");
+const div = document.createElement("div");
+
+renderClock();
+
+setInterval(() => {
+    headerTop.firstElementChild.innerHTML = `<span>` + getformatedDate() + `</span> ` + `<span>` + getTime() + `</span>`;
+}, 1000)
+
+
+
+//Functions
+function renderClock() {
+    div.className = "clock";
+    headerTop.prepend(div);
+
+    headerTop.firstElementChild.innerHTML = `<span>` + getformatedDate() + `</span> ` + `<span>` + getTime() + `</span>`;
+}
+
+
+function getformatedDate() {
+    const fullDate = new Date();
+    const date = fullDate.toDateString();
+    const dayNum = fullDate.getDate();
+    let suffix; 
+
+    switch (dayNum) {
+        case 1:
+        case 21:
+        case 31:
+            suffix = 'st'
+            break;
+        case 2:
+        case 22:
+            suffix = 'nd'
+            break;
+        case 3:
+        case 23:
+            suffix = 'rd'
+            break;
+        default:
+            suffix = 'th'
+            break;
+    }
+
+    return date.slice(0, 3) + ',' + date.slice(3, 8) + parseInt(date.slice(8, 10)) + suffix;
+
+}
+
+function getTime() {
+    const fullDate = new Date();
+    const hours = formatTime(fullDate.getHours());
+    const minutes = formatTime(fullDate.getMinutes());
+    const seconds = formatTime(fullDate.getSeconds());
+
+    return hours + ":" + minutes + ":" + seconds;
+}
+
+function formatTime(x) {
+    if (x < 10) x = "0" + x;
+    return x;
+}
